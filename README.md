28f2983b316447ae199d67fbab123bd93c23f6f6
mohsen1299@gmail.com <mohsen1299@gmail.com>
2025-02-25 21:23:41 +0330

    move commands to new project

---
+++ README.md
@@ -0,0 +1,35 @@
+# My Python Cli
+
+a simple python cli (command line interface) for helping to make python project and other stuff.
+
+## Use cli
+
+- clone github repo on your machine
+
+```sh
+git clone https://github.com/mohsen12999/my-python-cli.git 
+```
+
+- install app: `uv tool install . -e`
+
+- `mpy -v` or `python -m mpy -v`: to show app version.
+- `mpy --help`: to show application help.
+- `mpy new`: a type of project from list.
+- `mpy env`: add and active virtual env.
+- `mpy req`:  make requirement file.
+- `mpy i` or `mpy install`: install dependency.
+- `mpy gi` or `mpy gitignore`: add gitignore to project.
+
+## Todo Ideas
+
+- nothing left to do.
+
+## commands
+
+- make project with `uv init --app --package mpy`
+
+## References
+
+- [Build a Command-Line To-Do App With Python and Typer](https://realpython.com/python-typer-cli/)
+- [How to build a CLI Tool using Python?](https://episyche.com/blog/how-to-build-a-cli-tool-using-python)
+- [The easy (and nice) way to do CLI apps in Python](https://trstringer.com/easy-and-nice-python-cli/)
---
+++ src/mpy/cli.py
@@ -0,0 +1,91 @@
+"""This module provides the MyPy CLI."""
+
+from typing import Optional
+
+import os
+import typer
+import questionary
+
+from mpy import __app_name__, __version__
+
+app = typer.Typer()
+
+def _version_callback(value: bool) -> None:
+    if value:
+        typer.echo(f"{__app_name__} v{__version__}")
+        raise typer.Exit()
+
+@app.callback()
+def main(
+    version: Optional[bool] = typer.Option(
+        None,
+        "--version",
+        "-v",
+        help="Show the application's version and exit.",
+        callback=_version_callback,
+        is_eager=True,
+    )
+) -> None:
+    return
+
+@app.command()
+def new(project_type: str = typer.Argument(None, help="choose a project type")) -> None:
+    """choose a project type"""
+
+    options = {
+        "Basic": "mkdir my_project && cd my_project && echo 'print(\"Hello, World!\")' > main.py",
+        "Django": "pip install django && django-admin startproject my_project",
+        "Flask": "pip install Flask && mkdir my_project && cd my_project && echo 'from flask import Flask\napp = Flask(__name__)\n@app.route('/')\ndef hello_world():\n\treturn 'Hello, World!'\nif __name__ == '__main__':\n\tapp.run(debug=True)'\nif __name__ == '__main__':\n\tapp.run(debug=True)' > app.py",
+        "FastAPI": """pip install fastapi uvicorn && mkdir my_project && cd my_project && echo "from fastapi import FastAPI\nimport uvicorn\napp = FastAPI()\n@app.get('/')\ndef read_root():\n\treturn {'message': 'Hello, World!'}\nif __name__ == '__main__':\n\tuvicorn.run(app, host='0.0.0.0', port=8000)" > main.py""",
+        "Scrapy": "pip install scrapy && mkdir my_project && cd my_project && echo 'import scrapy\nclass MySpider(scrapy.Spider):\n\tname = 'my_spider'\n\tstart_urls = ['https://example.com']\n\tdef parse(self, response):\n\t\tprint(response.text)' > main.py",
+        "Data Science": "mkdir my_project && cd my_project && pip install jupyter pandas numpy matplotlib seaborn && echo 'import pandas as pd\nimport numpy as np\nimport matplotlib.pyplot as plt\nimport seaborn as sns\nimport jupyter\nprint(\"Hello, World!\")' > main.py",
+        "Machine Learning": "mkdir my_project && cd my_project && pip install numpy pandas scikit-learn tensorflow torch && echo 'import numpy as np\nimport pandas as pd\nimport scikit-learn\nimport tensorflow as tf\nimport torch\nprint(\"Hello, World!\")' > main.py",
+    }
+
+    if project_type == None or project_type not in options:
+
+        project_type = questionary.select(
+            "What do you want to do?",
+            choices=options.keys()).ask()
+
+    project_name = questionary.text("What's your Project name? [my_project]", default="my_project").ask()
+
+    if project_name == None:
+        project_name = "my_project"
+
+    our_command = options[project_type].replace("my_project", project_name)
+
+    os.system(our_command)
+    typer.echo("Project created.")
+
+
+@app.command()
+def rq():
+    """make requirement file"""
+    os.system("pip freeze > requirements.txt")
+    typer.echo("Requirement file created.")
+
+@app.command()
+def env():
+    """add and active virtual env"""
+    os.system("python -m venv venv")
+    if os.name == "nt":
+        os.system("venv\\Scripts\\activate")
+    else:
+        os.system("source venv/bin/activate")
+    
+    typer.echo("Virtual environment activated.")
+
+@app.command("i")
+@app.command("install")
+def install():
+    """install dependency"""
+    os.system("pip install -r requirements.txt")
+    typer.echo("Dependencies installed.")
+
+@app.command("gi")
+@app.command("gitignore")
+def gitignore():
+    """add gitignore to project from github"""
+    os.system("curl -o .gitignore https://raw.githubusercontent.com/github/gitignore/main/Python.gitignore")
+    typer.echo(".gitignore file created from github template.")
