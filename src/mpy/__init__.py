5de2c99faeb9f158999e2cc54b166a4e5dafd661
mohsen1299@gmail.com <mohsen1299@gmail.com>
2025-02-25 22:22:47 +0330

    update project and add __main__.py

--- src/mpy/__init__.py
+++ src/mpy/__init__.py
@@ -1,12 +1,4 @@
-"""mpy entry point script."""
+"""Top-level package for MPy."""
 
 __app_name__ = "MPy"
 __version__ = "0.2.0"
-
-from mpy import cli
-
-def main():
-    cli.app(prog_name=__app_name__)
-
-if __name__ == "__main__":
-    main()
